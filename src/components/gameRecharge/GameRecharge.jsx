62aefa878fde35192b6bcc86e162ed163124ad39
vatiza <rakibur.mac@gmail.com>
2024-10-30 00:55:17 +0600

    button show and hidden

--- src/components/gameRecharge/GameRecharge.jsx
+++ src/components/gameRecharge/GameRecharge.jsx
@@ -5,14 +5,22 @@ import ProductsCard from "../shared/productsCard/ProductsCard";
 const GameRecharge = () => {
   return (
     <div className="mt-4">
-      <div className="flex justify-between">
-        <h1 className="text-2xl uppercase font-bold">Games Recharge</h1>
-        <Button color="primary" variant="solid">
+      <div className="text-center lg:flex justify-between">
+        <h1 className="text-2xl uppercase font-bold ">Games Recharge</h1>
+        <Button className="hidden lg:flex" color="primary" variant="solid">
           <Link to="/">More Products</Link>
         </Button>
       </div>
       <Divider className="my-2" />
       <ProductsCard />
+
+      <Button
+        className="flex mx-auto lg:hidden  my-2 "
+        color="primary"
+        variant="solid"
+      >
+        <Link to="/">More Products</Link>
+      </Button>
     </div>
   );
 };
